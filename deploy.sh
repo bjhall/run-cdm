0e482ccb996de1e7234f1443435b15a810b7674a
Björn Hallström <bjorn.hallstrom@gmail.com>
2019-11-14 08:01:36 +0100

    Show diff at deployment

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,20 @@
-cp parse_statsjson.pl /data/bnf/scripts
-cp run_stats.pl /data/bnf/scripts
+
+for f in parse_statsjson.pl run_stats.pl
+do
+    echo "########## DIFF: $f #############"
+    colordiff $f /data/bnf/scripts/$f
+done
+
+read -p "Have you tested everything???? " -n 1 -r
+
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "Deploying..."
+    cp parse_statsjson.pl /data/bnf/scripts
+    cp run_stats.pl /data/bnf/scripts
+    echo "DONE!"
+fi
+
+
+
